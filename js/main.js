35a0c6d5b7465e905b7e2f490acee4316eea265c
Screamo95 <bykovglobal@gmail.com>
2024-06-04 10:24:58 +0300

    Вертска projects, модальное окно header

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// Модальное окно header
+
+Swal.mixin({
+  title: "Хочешь работать со мной?",
+  text: "Позвони мне или напиши на мою почту!",
+  icon: "question",
+  confirmButtonText: `<a href="tel:+79871452718">Позвонить</a>`,
+  confirmButtonColor: "#34547A",
+  showDenyButton: true,
+  denyButtonText: `<a href="mailto:experents23@gmail.com">Написать</a>`,
+  denyButtonColor: "#34547A",
+  focusDeny: true,
+  showCancelButton: true,
+  cancelButtonText: "Отмена"
+}).bindClickHandler("data-swal-template");
+
 // Видеоплеер
 
 let playButton = document.querySelector(`#video__play`);
